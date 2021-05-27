bf57c935a4907e2a7123caa67829a99e92db3a72
nvpadmawar <nvpadmawar@gmail.com>
2021-05-27 11:50:50 +0530

    addition

---
+++ operation.java
@@ -0,0 +1,17 @@
+class operation{
+ pubblic static void main (string[] args){
+   
+  int a,b,c;
+   void add(){
+    
+   System.out.println("Enter a value : "+a);
+   System.out.println("Enter b value : "+b);
+   c=a+b;
+   System.out.println(" value of c: "+c);
+
+
+    }
+
+  }
+
+}
